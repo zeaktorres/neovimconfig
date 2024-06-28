d6e53cdcec654564249b109a60a8383bf0ea21e0
Ezequiel Torres <torreszeaks@gmail.com>
2024-06-27 22:16:54 -0700

    Added nvim config.

--- after/ftplugin/gdscript.lua
+++ after/ftplugin/gdscript.lua
@@ -1,36 +1,4 @@
-local port = os.getenv("GDScript_Port") or "6005"
-local cmd = vim.lsp.rpc.connect("127.0.0.1", port)
-local pipe = "/tmp/godot.pipe"
-
-vim.lsp.start({
-	name = "Godot",
-	cmd = cmd,
-	root_dir = vim.fs.dirname(vim.fs.find({ "project.godot", ".git" }, { upward = true })[1]),
-	on_attach = function(client, bufnr)
-		vim.api.nvim_command('echo serverstart("' .. pipe .. '")')
-	end,
-})
-local dap = require("dap")
-
-vim.keymap.set("n", "<F5>", dap.continue, { desc = "Debug: Start/Continue" })
-vim.keymap.set("n", "<leader>b", dap.toggle_breakpoint, { desc = "Debug: Toggle Breakpoint" })
-vim.keymap.set("n", "<leader>B", function()
-	dap.set_breakpoint(vim.fn.input("Breakpoint condition: "))
-end, { desc = "Debug: Set Breakpoint" })
-
-dap.adapters.godot = {
-	type = "server",
-	host = "127.0.0.1",
-	port = 6006,
-}
-
-dap.configurations.gdscript = {
-	{
-		type = "godot",
-		request = "launch",
-		name = "Launch scene",
-		project = "${workspaceFolder}",
-		launch_scene = true,
-	},
-}
-vim.opt.expandtab = false
+local pipepath = vim.fn.stdpath("cache") .. "/server.pipe"
+if not vim.loop.fs_stat(pipepath) then
+  vim.fn.serverstart(pipepath)
+end
--- after/plugin/null-ls.lua
+++ after/plugin/null-ls.lua
@@ -4,7 +4,7 @@ null_ls.setup({
 	sources = {
 		null_ls.builtins.formatting.stylua,
 		null_ls.builtins.diagnostics.eslint,
-		null_ls.builtins.completion.spell,
+        --null_ls.builtins.completion.spell,
 		null_ls.builtins.diagnostics.cpplint,
 		null_ls.builtins.formatting.clang_format,
 		null_ls.builtins.diagnostics.checkmake,
